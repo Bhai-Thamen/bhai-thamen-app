42f553aa663da1700dc5212bd1162318201cf753
Luke Doyle <luke@caffebd.org>
2020-12-15 21:33:25 +0600

    map menu updated

--- lib/screens/map_top_menu.dart
+++ lib/screens/map_top_menu.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+var allIcons = ['toilets', 'medical'];
+
 class MapTopMenu extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -11,49 +13,27 @@ class MapTopMenu extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
           Padding(
-            padding:
-                EdgeInsets.only(top: MediaQuery.of(context).viewInsets.top),
-            child: const Text("Choose Wisely",
-                style: TextStyle(color: Colors.white, fontSize: 20),
-                textAlign: TextAlign.center),
-          ),
-          Padding(
             padding: const EdgeInsets.only(bottom: 5),
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: <Widget>[
-                Expanded(
-                    child: Padding(
-                  padding: const EdgeInsets.only(left: 10, right: 5),
-                  child: OutlineButton(
-                    padding: const EdgeInsets.symmetric(vertical: 15),
-                    child: Image.network(
-                      "https://www.fifaindex.com/static/FIFA21/images/crest/10/light/1878.webp",
-                      height: MediaQuery.of(context).size.height * .15,
+                for (var i = 0; i < 2; i++)
+                  Expanded(
+                      child: Padding(
+                    padding: const EdgeInsets.only(left: 2, right: 2),
+                    child: OutlineButton(
+                      padding: const EdgeInsets.all(3),
+                      child: Image.asset(
+                        'assets/images/' + allIcons[i] + '.png',
+                        width: MediaQuery.of(context).size.width / 5,
+                      ),
+                      splashColor: Colors.white,
+                      highlightedBorderColor: Colors.teal,
+                      onPressed: () {
+                        Navigator.of(context).pop(allIcons[i]);
+                      },
                     ),
-                    splashColor: Colors.white,
-                    highlightedBorderColor: Colors.teal,
-                    onPressed: () {
-                      Navigator.of(context).pop("medical");
-                    },
-                  ),
-                )),
-                Expanded(
-                    child: Padding(
-                  padding: const EdgeInsets.only(left: 5, right: 10),
-                  child: OutlineButton(
-                    padding: const EdgeInsets.symmetric(vertical: 15),
-                    child: Image.network(
-                      "https://www.fifaindex.com/static/FIFA20/images/crest/10/light/1028.webp",
-                      height: MediaQuery.of(context).size.height * .15,
-                    ),
-                    splashColor: Colors.white,
-                    highlightedBorderColor: Colors.teal,
-                    onPressed: () {
-                      Navigator.of(context).pop("toilets");
-                    },
-                  ),
-                ))
+                  )),
               ],
             ),
           ),
